548f71666d462293247bcbbf2e975df550aced50
Max <bespalov.m.9@ya.ru>
2024-02-12 22:37:31 +0300

    status bar color was changed in theme.kt

--- app/src/main/java/bes/max/mobinttest/core/ui/theme/Theme.kt
+++ app/src/main/java/bes/max/mobinttest/core/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -27,7 +28,7 @@ private val LightColorScheme = lightColorScheme(
     tertiary = Pink40,
     background = White,
 
-    )
+)
 
 @Composable
 fun MobintTestTheme(
@@ -42,15 +43,15 @@ fun MobintTestTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
+        darkTheme -> LightColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = Color.White.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
         }
     }
 
